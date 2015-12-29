fae4dfe51740dbbd532c1d5379844df5ef5f1167
Daniel Grassinger <DanielGrassinger@gmail.com>
2015-12-29 18:52:44 +0100

    Create main

---
+++ Simple-3D-Scanner/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 29.12.2015
+ *      Author: daniel
+ */
+
+#include<iostream>
+
+using namespace std;
+
+
+int main(int argc, char argv[]){
+	cout << "Hallo Welt" << endl;
+
+	return 0;
+}
