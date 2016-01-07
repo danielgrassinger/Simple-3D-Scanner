e99eb0a88f6c8cfb713830a876059f7583039bf4
Daniel Grassinger <DanielGrassinger@gmail.com>
2016-01-07 20:56:36 +0100

    Include CMake

--- Simple-3D-Scanner/src/main.cpp
+++ Simple-3D-Scanner/src/main.cpp
@@ -7,15 +7,18 @@
 
 #include<iostream>
 
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
+//include "opencv2/core/core.hpp"
+//include "opencv2/highgui/highgui.hpp"
 
 
 int main(int argc, char argv[]){
-
+	/*
 	cv::Mat image = cv::imread("im.jpg");
 	cv::neamedWindow("My Image");
 	cv::imshow("My Image",image);
 	cv::waitKey(5000);
+	*/
+
+	std::cout << "Hallo Welt!xx!" << std::endl;
 	return 0;
 }
